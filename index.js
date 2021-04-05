23a21d4b211fa3653ffd1d89824f9d70b338b712
yoho4613 <79840170+yoho4613@users.noreply.github.com>
2021-04-05 20:05:37 +1200

    update

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
+// ================HTML SELECTORS ===============
+let body = document.querySelector("body");
 let retailerMainBtn = document.querySelector(".retailer-main-container-tags");
+let navList = document.querySelectorAll(".nav-list");
+let btnNav = document.querySelectorAll(".btn-nav");
+let dropdown = document.querySelectorAll(".service-dropdown");
+
+// ----------------addEventListeners----------------
+
+
 
 retailerMainBtn.addEventListener("click", function (e) {
   let tagName = e.target.className;
@@ -15,3 +24,18 @@ retailerMainBtn.addEventListener("click", function (e) {
     divs[2].classList.add("show");
   }
 });
+
+navList.forEach((el) =>
+  el.addEventListener("click", function (e) {
+    e.preventDefault();
+    if (e.target === btnNav[0]) {
+      dropdown[0].classList.toggle("hide");
+    }
+    if (e.target === btnNav[1]) {
+      dropdown[1].classList.toggle("hide");
+    }
+    if (e.target === btnNav[2]) {
+      dropdown[2].classList.toggle("hide");
+    }
+  })
+);
